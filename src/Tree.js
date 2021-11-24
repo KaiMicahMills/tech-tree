3f6bc5e0e3bca2996988d225f9ff4024eeee27ac
Kai Micah Mills <kaimicahmills@gmail.com>
2021-11-24 15:33:57 -0700

    initial scaffold

---
+++ src/Tree.js
@@ -0,0 +1,10 @@
+import './tech-tree.scss';
+
+const Tree = () => {
+
+  return (
+    <div class="styled">Longevity Tech Tree</div>
+  )
+}
+
+export default Tree;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Tree from "./Tree";
+
+/**
+ * Longevity Tech Tree v0.1
+ * Copyright 2021 Foresight Institute
+ */
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Tree />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
