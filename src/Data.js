0af85156260181263b95af22ea67ba07f9ad48df
Kai Micah Mills <kaimicahmills@gmail.com>
2022-01-24 19:34:32 -0700

    re-add longevity data

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,424 @@
 const Data = [
-
+  {
+    "title": "Epigenetics",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Senescence Control",
+    "type": "core-technology",
+    "relations": [
+      "Epigenetics"
+    ]
+  },
+  {
+    "title": "Regeneration of Senescent Cells",
+    "type": "longevity-tech",
+    "relations": [
+      "Senescence Control"
+    ]
+  },
+  {
+    "title": "Telomere Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Gene Repair Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Cure for Cancer",
+    "type": "general-improvement",
+    "relations": [
+      "Telomere Control",
+      "Senescence Control",
+      "Gene Repair Control"
+    ]
+  },
+  {
+    "title": "Electrodynamics",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Extracellular Matrix Reconstruction",
+    "type": "core-technology",
+    "relations": [
+      "Electrodynamics"
+    ]
+  },
+  {
+    "title": "Gene Therapy",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Gene Expression Control",
+    "type": "core-technology",
+    "relations": [
+      "Epigenetics",
+      "Electrodynamics",
+      "Gene Therapy"
+    ]
+  },
+  {
+    "title": "Circadian Rhythm Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Regeneration Cycles",
+    "type": "longevity-tech",
+    "relations": [
+      "Circadian Rhythm Control"
+    ]
+  },
+  {
+    "title": "Hormone Replacement",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Immune System Regeneration",
+    "type": "longevity-tech",
+    "relations": [
+      "Hormone Replacement"
+    ]
+  },
+  {
+    "title": "Heat Shock Response Control",
+    "type": "core-technology"
+  },
+  {
+    "title": "Apoptosis",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Hormesis",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Autophagy Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Glymphatic System Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Complete Detritus Recycling",
+    "type": "longevity-tech",
+    "relations": [
+      "Glymphatic System Control"
+    ]
+  },
+  {
+    "title": "Microbiome Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Extracellular Matrix Turnover",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Induced Hibernation",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Vitrified Body",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Cryonic Revival",
+    "type": "longevity-tech",
+    "relations": [
+      "Vitrified Body"
+    ]
+  },
+  {
+    "title": "External Organs",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Artificial Womb",
+    "type": "core-technology",
+    "relations": [
+      "External Organs"
+    ]
+  },
+  {
+    "title": "Body on a Chip",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Immunosenescence",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Skin Grafts",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Artificial Muscle",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Brain Machine Interface",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Lifelike Prosthetics",
+    "type": "general-improvement",
+    "relations": [
+      "Artificial Muscle",
+      "Brain Machine Interface"
+    ]
+  },
+  {
+    "title": "Head Transplant",
+    "type": "core-technology",
+    "relations": [
+      "Brain Machine Interface"
+    ]
+  },
+  {
+    "title": "Blood Reperfusion",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Brain Preservation",
+    "type": "core-technology",
+    "relations": [
+      "Blood Reperfusion"
+    ]
+  },
+  {
+    "title": "Mind Uploading",
+    "type": "core-technology",
+    "relations": [
+      "Brain Machine Interface",
+      "Brain Preservation"
+    ]
+  },
+  {
+    "title": "Cybernetic Brain",
+    "type": "core-technology",
+    "relations": [
+      "Mind Uploading"
+    ]
+  },
+  {
+    "title": "Brain Transplant",
+    "type": "core-technology",
+    "relations": [
+      "Brain Preservation"
+    ]
+  },
+  {
+    "title": "Body Replacement",
+    "type": "longevity-tech",
+    "relations": [
+      "Brain Transplant",
+      "Head Transplant"
+    ]
+  },
+  {
+    "title": "Reactive Oxygen Species",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Inflammaging",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Microbiome Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Stem Cell Therapy",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Lab Grown Blood",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Frozen Blood",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Blood Donations Solved",
+    "type": "general-improvement",
+    "relations": [
+      "Lab Grown Blood",
+      "Frozen Blood"
+    ]
+  },
+  {
+    "title": "Parabiosis",
+    "type": "longevity-tech",
+    "relations": [
+      "Frozen Blood"
+    ]
+  },
+  {
+    "title": "ATP to Electricity",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Electricity to ATP",
+    "type": "core-technology",
+    "relations": [
+      "ATP to Electricity"
+    ]
+  },
+  {
+    "title": "Food from Energy",
+    "type": "general-improvement",
+    "relations": [
+      "Electricity to ATP"
+    ]
+  },
+  {
+    "title": "Whitelisting Proteins",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Artificial Organs",
+    "type": "core-technology",
+    "relations": [
+      "ATP to Electricity"
+    ]
+  },
+  {
+    "title": "Cybernetic Body",
+    "type": "core-technology",
+    "relations": [
+      "Artificial Muscle",
+      "Artificial Organs"
+    ]
+  },
+  {
+    "title": "Cyborg",
+    "type": "core-technology",
+    "relations": [
+      "Cybernetic Body"
+    ]
+  },
+  {
+    "title": "Android",
+    "type": "longevity-tech",
+    "relations": [
+      "Cyborg",
+      "Cybernetic Brain"
+    ]
+  },
+  {
+    "title": "Proteolysis Control",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Advanced Glycation Endproducts",
+    "type": "core-technology",
+    "relations": [
+      ""
+    ]
+  },
+  {
+    "title": "Lipofuscin Degradation",
+    "type": "core-technology",
+    "relations": [
+      "Proteolysis Control"
+    ]
+  }
 ];
 
 export default Data;
